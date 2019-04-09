2b95b37e35df495edf22a851f7db5db9bd0730cd
anithaamarnath <anithaamarnath04@gmail.com>
2019-04-09 01:04:36 +0000

    this is fourth file

--- getprint4.js
+++ getprint4.js
@@ -1,20 +1,22 @@
-function getAndPrintHTML (options) {
-  var https = require('https');
+
+var https = require('https');
+//function getAndPrintHTML (options,actions) {
+
 
 // the host can be thought of as the domain name you want to read from,
 // and the path is the resource - '/' is the root path, but if you wanted to read a
 // particular resource (like '/login/index.html'), that would be defined in the path
 
-var options = {
+var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step2.html'
+  path: '/http-examples/step4.html'
 }
-function getAndPrintHTML (options) {
+function printHTML(data) {
+    console.log(data);
+}
+function getHTML (options,callback){
 
 
-
-// notice that https.get takes a callback with one parameter -
-// response, which is a Stream that represents the HTTP response
 https.get(requestOptions, function (response) {
 
   // set encoding of received data to UTF-8
@@ -24,27 +26,30 @@ https.get(requestOptions, function (response) {
   response.on('data', function (data) {
 
      chunk += data;
-
-    // console.log('Chunk Received:',data1);
-
   });
 
 
-  // the callback is invoked when all of the data has been received
-  // (the `end` of the stream)
-  response.on('end', function() {
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+
+
+function printHTML(data) {
+    console.log(data);
+}
+response.on('end', function() {
     console.log('Response stream complete.' + chunk);
   });
+
 });
-}
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
-}
+
 }
 
 
-getAndPrintHTML();
+getHTML(requestOptions,printHTML);
+
+
+
+
 
 
 
